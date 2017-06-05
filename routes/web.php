b4a8bbeb1147b526182cae820beb7b3f2eb4b82c
Murad Hajiyev <murad.hajiyev@atltech.az>
2017-06-05 09:59:17 +0400

    Modified web routes

--- routes/web.php
+++ routes/web.php
@@ -23,28 +23,13 @@ Route::get('/storeregister', function () {
     return view('auth/storeregister');
 });
 
-Route::get('/admin', 'AdminController@index');
 
 Route::group(['prefix' => 'admin'], function () {
-    Route::get('categories', function () {
-        return view('admin.categories');
-    });
 
+    Route::get('/', 'AdminController@index');
     Route::get('/categories', 'AdminController@categories');
-    //dropdowns route
     Route::resource('dropdowns', 'DropdownController');
     Route::resource('specifications', 'SpecificationController');
-   // Route::get('/dropdowns', 'DropdownsController@getDropdowns');
-//    Route::get('/dropdownsForm', 'DropdownsController@getDropdownsForm');
-//    Route::post('/addDropdown', 'DropdownsController@store');
-//    Route::get('deleteDropdown/{id}', ['as' => 'deleteD', 'uses' => 'DropdownsController@delete']);
-    //specifications route
-
-//    Route::get('/specifications', 'SpecificationsController@getSpecifications');
-//    Route::get('/showDropdowns', 'SpecificationsController@showDropdowns');
-//    Route::get('/specificationsForm', 'SpecificationsController@getSpecificationsForm');
-//    Route::post('/addSpecification', 'SpecificationsController@store');
-//    Route::get('deleteSpecification/{id}', ['as' => 'deleteS', 'uses' => 'SpecificationsController@delete']);
 
 });
 
