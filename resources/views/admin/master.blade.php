2af03ad7db69a963c29c596dede6b4a3f1f64366
Arsalan <airavani2018@ada.edu.az>
2017-06-07 10:45:07 +0400

    asd

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -1,51 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-
-   <meta charset="utf-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <title>Admin | Dashboard</title>
-   <!-- Tell the browser to be responsive to screen width -->
-   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
-   <!-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"> -->
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-   <!-- Font Awesome -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-   <!-- Ionicons -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-   <!-- Theme style -->
-   <link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css')}}">
-   <!-- AdminLTE Skins. Choose a skin from the css/skins
-   folder instead of downloading all of them to reduce the load. -->
-   <link rel="stylesheet" href="{{ asset('dist/css/skins/_all-skins.min.css') }}">
-   <!-- Morris chart -->
-   <link rel="stylesheet" href="{{ asset('plugins/morris/morris.css')}}">
-   <!-- jvectormap -->
-   <link rel="stylesheet" href="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">
-
-   <!-- Daterange picker -->
-   <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css')}}">
-   <!-- bootstrap wysihtml5 - text editor -->
-   <link rel="stylesheet" href="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
-
-   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-   <!--[if lt IE 9]>-->
-
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Admin | Dashboard</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.7 -->
+    {{--<!-- Bootstrap 3.3.7 -->--}}
+    {{--<!-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"> -->--}}
+    {{--<!-- Font Awesome -->--}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"> -->
-    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
@@ -53,49 +20,27 @@
     <!-- AdminLTE Skins. Choose a skin from the css/skins
     folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="{{ asset('dist/css/skins/_all-skins.min.css') }}">
-    <!-- Morris chart -->
-    <link rel="stylesheet" href="{{ asset('plugins/morris/morris.css')}}">
-    <!-- jvectormap -->
-    <link rel="stylesheet" href="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">
-    <!-- Date Picker -->
-    <link rel="stylesheet" href="{{ asset('plugins/datepicker/datepicker3.css')}}">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css')}}">
-    <!-- bootstrap wysihtml5 - text editor -->
+{{--<!-- Morris chart -->--}}
+{{--<link rel="stylesheet" href="{{ asset('plugins/morris/morris.css')}}">--}}
+{{--<!-- jvectormap -->--}}
+{{--<link rel="stylesheet" href="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">--}}
+{{--<!-- Date Picker -->--}}
+{{--<link rel="stylesheet" href="{{ asset('plugins/datepicker/datepicker3.css')}}">--}}
+{{--<!-- Daterange picker -->--}}
+{{--<link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css')}}">--}}
+<!-- bootstrap wysihtml5 - text editor -->
     <link rel="stylesheet" href="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>-->
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="{{asset("js/specification.js")}}"></script>
-    <script src="{{ asset("js/grid.js") }}"></script>
-    <!--store css-->
-    <link rel="stylesheet" href="{{ asset("/css/grid.css")}}">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-
-    <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
-
 
 <!--<link href="{{ asset('css/treeview.css') }}" rel="stylesheet">-->
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>-->
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+{{--<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>--}}
 
-    <!--<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>-->
+<!--<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>-->
 
     <script src="{{ asset('js/specification.js' )}}"></script>
-
 
 
 </head>
@@ -422,15 +367,6 @@
                         <span class="pull-right-container">
                   <!-- <small class="label pull-right bg-green">new</small> -->
                </span>
-             </a>
-                </li>
-                <li>
-                    <a href="{{ url('/storecontrol') }}">
-                        <i class="fa fa-th"></i> <span>Stores</span>
-                        <span class="pull-right-container">
-                  <!-- <small class="label pull-right bg-green">new</small> -->
-               </span>
-
                     </a>
                 </li>
             </ul>
@@ -449,14 +385,12 @@
 
 
         <!-- <footer class="main-footer">
-
         <div class="pull-right hidden-xs">
         <b>Version</b> 2.4.0
      </div>
      <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
      reserved.
      </footer> -->
-
 
         <!-- Control Sidebar -->
         <aside class="control-sidebar control-sidebar-dark">
@@ -475,7 +409,6 @@
                             <a href="javascript:void(0)">
                                 <i class="menu-icon fa fa-birthday-cake bg-red"></i>
 
-
                                 <div class="menu-info">
                                     <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
 
@@ -486,7 +419,6 @@
                         <li>
                             <a href="javascript:void(0)">
                                 <i class="menu-icon fa fa-user bg-yellow"></i>
-
 
                                 <div class="menu-info">
                                     <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
@@ -635,7 +567,6 @@
                                 <input type="checkbox" class="pull-right">
                             </label>
                         </div>
-
                         <!-- /.form-group -->
 
                         <div class="form-group">
@@ -650,72 +581,6 @@
                 </div>
                 <!-- /.tab-pane -->
             </div>
-
-            <!-- /.form-group -->
-
-            <div class="form-group">
-               <label class="control-sidebar-subheading">
-                  Delete chat history
-                  <a href="javascript:void(0)" class="text-red pull-right"><i
-                     class="fa fa-trash-o"></i></a>
-                  </label>
-               </div>
-               <!-- /.form-group -->
-            </form>
-         </div>
-         <!-- /.tab-pane -->
-      </div>
-   </aside>
-   <!-- /.control-sidebar -->
-   <!-- Add the sidebar's background. This div must be placed
-   immediately after the control sidebar -->
-   <div class="control-sidebar-bg"></div>
-</div>
-
-<!-- <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script> -->
-<!-- <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> -->
-
-<!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-$.widget.bridge('uibutton', $.ui.button);
-</script>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<!-- Morris.js charts -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script> -->
-<!-- <script src="{{ asset('plugins/morris/morris.min.js')}}"></script> -->
-<!-- Sparkline -->
-<!-- <script src="{{ asset('plugins/sparkline/jquery.sparkline.min.js')}}"></script> -->
-<!-- jvectormap -->
-<!-- <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script> -->
-<!-- <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script> -->
-<!-- jQuery Knob Chart -->
-<!-- <script src="{{ asset('plugins/knob/jquery.knob.js')}}"></script> -->
-<!-- daterangepicker -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script> -->
-<!-- <script src="{{ asset('plugins/daterangepicker/daterangepicker.js')}}"></script> -->
-<!-- datepicker -->
-<!-- <script src="{{ asset('plugins/datepicker/bootstrap-datepicker.js')}}"></script> -->
-<!-- Bootstrap WYSIHTML5 -->
-<!-- <script src="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}"></script> -->
-<!-- Slimscroll -->
-<!-- <script src="{{ asset('plugins/slimScroll/jquery.slimscroll.min.js')}}"></script> -->
-<!-- FastClick -->
-<!-- <script src="{{ asset('plugins/fastclick/fastclick.js')}}"></script> -->
-<!-- AdminLTE App -->
-<!-- <script src="{{ asset('dist/js/adminlte.min.js')}}"></script> -->
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<!-- <script src="{{ asset('dist/js/pages/dashboard.js')}}"></script> -->
-<!-- AdminLTE for demo purposes -->
-<!-- <script src="{{ asset('dist/js/demo.js')}}"></script> -->
-
-<script src="{{ asset('js/specification.js' )}}"></script>
-<script src="{{ asset('/js/treeview.js') }}"></script>
-
         </aside>
         <!-- /.control-sidebar -->
         <!-- Add the sidebar's background. This div must be placed
@@ -723,55 +588,49 @@ $.widget.bridge('uibutton', $.ui.button);
         <div class="control-sidebar-bg"></div>
     </div>
     <!-- ./wrapper -->
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <!-- jQuery 3.1.1 -->
-    <script src="{{ asset('plugins/jQuery/jquery-3.1.1.min.js')}}"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+{{--<script src="{{ asset('plugins/jQuery/jquery-3.1.1.min.js')}}"></script>--}}
+<!-- jQuery UI 1.11.4 -->
+{{--<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>--}}
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
         $.widget.bridge('uibutton', $.ui.button);
     </script>
-    <!-- Bootstrap 3.3.7 -->
-    <script src="{{ asset('js/bootstrap.min.js')}}"></script>
-    <!-- Morris.js charts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-    <script src="{{ asset('plugins/morris/morris.min.js')}}"></script>
-    <!-- Sparkline -->
-    <script src="{{ asset('plugins/sparkline/jquery.sparkline.min.js')}}"></script>
-    <!-- jvectormap -->
-    <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
-    <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
-    <!-- jQuery Knob Chart -->
-    <script src="{{ asset('plugins/knob/jquery.knob.js')}}"></script>
-    <!-- daterangepicker -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-    <script src="{{ asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
-    <!-- datepicker -->
-    <script src="{{ asset('plugins/datepicker/bootstrap-datepicker.js')}}"></script>
-    <!-- Bootstrap WYSIHTML5 -->
-    <script src="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}"></script>
-    <!-- Slimscroll -->
-    <script src="{{ asset('plugins/slimScroll/jquery.slimscroll.min.js')}}"></script>
-    <!-- FastClick -->
-    <script src="{{ asset('plugins/fastclick/fastclick.js')}}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{ asset('dist/js/adminlte.min.js')}}"></script>
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="{{ asset('dist/js/pages/dashboard.js')}}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{ asset('dist/js/demo.js')}}"></script>
-<!-- <script src="{{ asset('/js/adminCustomJS.js')}}"></script> -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+{{--<!-- Bootstrap 3.3.7 -->--}}
+{{--<script src="{{ asset('js/bootstrap.min.js')}}"></script>--}}
+{{--<!-- Morris.js charts -->--}}
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>--}}
+{{--<script src="{{ asset('plugins/morris/morris.min.js')}}"></script>--}}
+{{--<!-- Sparkline -->--}}
+{{--<script src="{{ asset('plugins/sparkline/jquery.sparkline.min.js')}}"></script>--}}
+{{--<!-- jvectormap -->--}}
+{{--<script src="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>--}}
+{{--<script src="{{ asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>--}}
+{{--<!-- jQuery Knob Chart -->--}}
+{{--<script src="{{ asset('plugins/knob/jquery.knob.js')}}"></script>--}}
+{{--<!-- daterangepicker -->--}}
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>--}}
+{{--<script src="{{ asset('plugins/daterangepicker/daterangepicker.js')}}"></script>--}}
+{{--<!-- datepicker -->--}}
+{{--<script src="{{ asset('plugins/datepicker/bootstrap-datepicker.js')}}"></script>--}}
+<!-- Bootstrap WYSIHTML5 -->
+    {{--<script src="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}"></script>--}}
+    {{--<!-- Slimscroll -->--}}
+    {{--<script src="{{ asset('plugins/slimScroll/jquery.slimscroll.min.js')}}"></script>--}}
+    {{--<!-- FastClick -->--}}
+    {{--<script src="{{ asset('plugins/fastclick/fastclick.js')}}"></script>--}}
+    {{--<!-- AdminLTE App -->--}}
+    {{--<script src="{{ asset('dist/js/adminlte.min.js')}}"></script>--}}
+    {{--<!-- AdminLTE dashboard demo (This is only for demo purposes) -->--}}
+    {{--<script src="{{ asset('dist/js/pages/dashboard.js')}}"></script>--}}
+    {{--<!-- AdminLTE for demo purposes -->--}}
+    {{--<script src="{{ asset('dist/js/demo.js')}}"></script>--}}
+    {{--<!-- <script src="{{ asset('/js/adminCustomJS.js')}}"></script> -->--}}
 
     <script src="{{ asset('/js/treeview.js') }}"></script>
 
-
-
 </div>
-       </div>
 </body>
 </html>
