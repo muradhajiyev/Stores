0989b16a4e88b4bef8f33e361248813038ea7f79
Azad <you@example.com>
2017-06-07 15:19:53 +0400

    price-range.css linked inside main.blade.php

--- resources/views/store/products.blade.php
+++ resources/views/store/products.blade.php
@@ -1,4 +1,3 @@
-
 <section>
         <div class="container">
             <div class="row">
@@ -130,8 +129,8 @@
                         <div class="price-range"><!--price-range-->
                             <h2>Price Range</h2>
                             <div class="well text-center">
-                                <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2" style="width: 100%;"><br />
-                                <b class="pull-left">$ 0</b> <b class="pull-right">$ 600</b>
+                                 <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2" ><br />
+                                 <b class="pull-left">$ 0</b> <b class="pull-right">$ 600</b>
                             </div>
                         </div><!--/price-range-->
 
