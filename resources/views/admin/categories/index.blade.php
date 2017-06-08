73f5ff7bd33d24957bdea791958224c9410a34a8
JaviDragneel <jmammadov2018@ada.edu.az>
2017-06-08 12:56:36 +0400

    delete is added

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -12,6 +12,13 @@
                            class="fa fa-plus" aria-hidden="true"></i>
                         @if(count($category->childs))
                             @include('admin.categories.manageChild',['childs' => $category->childs])
+
+                        @else
+                            <form action="/admin/categories/{{$category->id}}" style="display: inline" method="Post">
+                                {{csrf_field()}}
+                                {{ method_field('DELETE') }}
+                                <button type="submit"><i class="fa fa-minus" aria-hidden="true"></i></button>
+                            </form>
                         @endif
                     </li>
                 @endforeach
