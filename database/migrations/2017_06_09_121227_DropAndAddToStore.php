2114a272fd13ce0ff3192021389c9d2309293c1d
Elnur1996 <ehajizade2018@ada.edu.az>
2017-06-09 12:37:57 +0400

    TimeStanmps added to stores table

--- database/migrations/2017_06_09_121227_DropAndAddToStore.php
+++ database/migrations/2017_06_09_121227_DropAndAddToStore.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateStore extends Migration
+class DropAndAddToStore extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class UpdateStore extends Migration
      */
     public function up()
     {
-        //
+     Schema::table('stores', function (Blueprint $table) {
+    $table->dropColumn(['created_at', 'updated_at']); 
+        });
     }
+    
 
     /**
      * Reverse the migrations.
@@ -23,8 +26,6 @@ class UpdateStore extends Migration
      */
     public function down()
     {
-        Schema::table('stores', function (Blueprint $table) {
-    $table->dropColumn(['created_at', 'updated_at']);
-});
+        //
     }
 }
--- database/migrations/2017_06_09_123500_Add_TimeStamps_to_StoresTable.php
+++ database/migrations/2017_06_09_123500_Add_TimeStamps_to_StoresTable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddToStore extends Migration
+class AddTimeStampsToStoresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class AddToStore extends Migration
      */
     public function up()
     {
-         Schema::table('stores', function (Blueprint $table) {
+        Schema::table('stores', function (Blueprint $table) {
             $table->timestamps();
-        });
-    }
+        });    }
 
     /**
      * Reverse the migrations.
