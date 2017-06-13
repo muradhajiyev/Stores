2175de6bd03f8d1c715f43431e8a3f557453feac
Murad Hajiyev <murad.hajiyev@atltech.az>
2017-06-13 17:19:12 +0400

    fixed append json value bug

--- app/Store.php
+++ app/Store.php
@@ -20,9 +20,10 @@ class Store extends Model
     	return $this->hasOne(Image::class, 'id', 'profile_image_id');
     }
 
-        public function getProfileUrlAttribute()
+    public function getProfileUrlAttribute()
     {
-        return config('settings.base_image_url').config('settings.store_profile_path').$this->profile_image->path;
+
+        return config('settings.base_image_url').config('settings.store_profile_base_path').$this->profile_image->path;
     }
 
 }
