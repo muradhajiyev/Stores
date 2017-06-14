bef64f4d5e8eb9b6d8d1894304802087719fb13b
saria17 <snuriyeva2018@ada.edu.az>
2017-06-14 20:41:35 +0400

    some unnecessary js duplications deleted in admin master.blade

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -709,5 +709,27 @@
             </div>
         </div>
     </section>
+    <!-- Modal -->
+    <div class="modal fade" id="advancedSearchModal" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Advanced search</h4>
+                </div>
+                <div class="modal-body">
+                    <form>
+
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
 @endsection
 @endif
