853d9fce26083fca2c718930a889d0f9a6e67431
Saleh Majidov <salehmecidovbaku@gmail.com>
2017-06-19 09:56:23 +0400

    Colour Fixed

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,7 +17,7 @@
     <link href="{{asset("product/css/responsive.css")}}" rel="stylesheet">
     <link href="{{asset("css/dropzone.css")}}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{asset("product/css/price-range.css")}}">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  
 
 
     <link rel="shortcut icon" href="images/ico/favicon.ico">
