f72f3b795eb897264d4e73a039781d4216bf9153
gnovruzov2018 <gnovruzov2018@ada.edu.az>
2017-06-21 10:21:37 +0400

    Autocomplete with category filter

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -68,8 +68,6 @@ class HomeController extends Controller
         $store_id = $request->input('store_id');
         $searchProduct = $request->input('searchStoreName');
         $category_id = $request->input('id');
-        Session::put('store_id1', $store_id);
-        Session::put('category_id_product', $category_id);
         $store = Store::find($store_id);
         $parentCategories = Category::all()->where('parent_id', null);
         $brands = Brand::all();
@@ -89,6 +87,8 @@ class HomeController extends Controller
                 $store->setRelation('products', $store->products()->where('name', 'like', '%' . $searchProduct . '%')->paginate(10));
             }
         }
+        Session::put('store_id1', $store_id);
+        Session::put('category_id_product', $category_id);
         return view('store.index', ['store' => $store, 'categories' => $parentCategories, 'brands' => $brands, 'mostviewed' => $product, 'subCategories' => $subCategories]);
     }
 
@@ -108,21 +108,21 @@ class HomeController extends Controller
         $category_id = Session::get('category_id_store');
         if (!is_null($category_id)){
             $store_ids = Product::where('category_id',$category_id)->pluck('store_id')->toArray();
-            $result = Store::whereIn('id',$store_ids)->where('name','LIKE','%'.$query.'%')->get();
+            $result = Store::whereIn('id',$store_ids)->where('name','LIKE','%'.$query.'%')->pluck('name');
         }else {
-            $result = Store::where('name', 'LIKE', '%' . $query . '%')->get();
+            $result = Store::where('name', 'LIKE', '%' . $query . '%')->pluck('name');
         }
         return response()->json($result);
     }
     public function autocompleteProduct(Request $request)
     {
-        $query = $request->get('query1','');
+        $query = $request->get('query','');
         $category_id = Session::get('category_id_product');
         $store_id = Session::get('store_id1');
         if (!is_null($category_id)){
-            $result = Product::where('store_id',$store_id)->where('category_id',$category_id)->where('name','LIKE','%'.$query.'%')->get();
+            $result = Product::where('store_id',$store_id)->where('category_id',$category_id)->where('name','LIKE','%'.$query.'%')->pluck('name');
         }else {
-            $result = Product::where('store_id',$store_id)->where('name','LIKE','%'.$query.'%')->get();
+            $result = Product::where('store_id',$store_id)->where('name','LIKE','%'.$query.'%')->pluck('name');
         }
         return response()->json($result);
     }
